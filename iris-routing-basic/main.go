2087e1b634b53cca2dea9df8e6c019295890380d
handuy1992 <handuy1992@gmail.com>
2019-08-06 15:04:08 +0700

    update ví dụ routing

--- iris-routing-basic/main.go
+++ iris-routing-basic/main.go
@@ -8,8 +8,13 @@ func homeHandler(ctx iris.Context) {
 	ctx.HTML("<h1>Chào mừng đến với Techmaster Việt Nam</h1>")
 }
 
+func courseHandler(ctx iris.Context) {
+	ctx.HTML("<h1>Danh sách khóa học của Techmaster Việt Nam</h1>")
+}
+
 func main() {
 	app := iris.New()
 	app.Handle("GET", "/", homeHandler)
+	app.Handle("GET", "/khoa-hoc", courseHandler)
 	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
