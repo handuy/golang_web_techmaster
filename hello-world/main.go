799cea4d539144942470703b4d247be22924e55e
handuy1992 <handuy1992@gmail.com>
2019-08-07 07:37:29 +0700

    cập nhật ví dụ hello-world

--- hello-world/main.go
+++ hello-world/main.go
@@ -5,7 +5,10 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "<h1 style='color:red;'>Web server chạy bằng Golang</h1>")
+    htmlH1String := "<h1 style='color:red'>Web server chạy bằng Golang</h1>"
+    htmlH2String := "<h2 style='color:green; font-size:20px; font-style:Italic;'>Học lập trình back-end</h2>"
+    returnHTML := htmlH1String + htmlH2String
+    fmt.Fprint(w, returnHTML)
 }
 
 func main() {
