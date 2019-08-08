0dd237dc761ac81c3b5bed3b82500615c8110426
handuy1992 <handuy1992@gmail.com>
2019-08-08 09:21:24 +0700

    tạo global middleware

--- iris-middleware/main.go
+++ iris-middleware/main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"log"
 	"time"
 
 	"github.com/kataras/iris"
 )
 
-func myMiddleware(ctx iris.Context) {
-	ctx.Application().Logger().Infof("Thời gian nhận request: %s", time.Now())
+func before(ctx iris.Context) {
+	log.Printf("Tên đường dẫn: %s. Thời gian nhận request: %s", ctx.Path(), time.Now())
 	ctx.Next()
+}
+
+func mainHandler(ctx iris.Context) {
+	ctx.HTML("<h1>Trang chủ</h1>")
+}
+
+func courseHandler(ctx iris.Context) {
+	ctx.HTML("<h1>Trang danh sách khóa học</h1>")
 }
 
 func main() {
 	app := iris.New()
-	app.Use(myMiddleware)
-	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("<h1>Tạo middleware với IRIS framework</h1>")
-	})
+	// Đăng kí hàm before làm middleware cho cả 2 route / và /khoa-hoc
+	app.Use(before)
+	app.Get("/", mainHandler)
+	app.Get("/khoa-hoc", courseHandler)
 	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
