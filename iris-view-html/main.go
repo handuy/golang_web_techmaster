90aa30b36b0a426c09b17325afe4dd19e2109c3b
handuy1992 <handuy1992@gmail.com>
2019-08-08 15:54:17 +0700

    cập nhật ví dụ view HTML

--- iris-view-html/main.go
+++ iris-view-html/main.go
@@ -1,12 +1,41 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"fmt"
+
+	"github.com/kataras/iris"
+)
+
+func homeHandler(ctx iris.Context) {
+	// Render file index.html trong thư mục ./view để trả về cho client
+	ctx.View("index.html")
+}
+
+func blogHandler(ctx iris.Context) {
+	// Render file index.html trong thư mục ./view/blog để trả về cho client
+	ctx.View("blog/index.html")
+}
+
+func serveFileHandler(ctx iris.Context) {
+	folder := ctx.Params().Get("folder")
+	file := ctx.Params().Get("file")
+	// đường dẫn trỏ đến file trong thư mục assets
+	filePath := fmt.Sprintf("./assets/%s/%s", folder, file)
+	ctx.ServeFile(filePath, false)
+}
 
 func main() {
 	app := iris.New()
+	// Đăng kí thư mục chứa file HTML
+	// ./view là đường dẫn đến thư mục view
+	// .html là định dạng các file HTML trả về
 	app.RegisterView(iris.HTML("./view", ".html"))
-	app.Get("/", func(ctx iris.Context) {
-		ctx.View("index.html")
-	})
+
+	app.Get("/", homeHandler)
+
+	app.Get("/bai-viet", blogHandler)
+
+	app.Get("/static/{folder}/{file}", serveFileHandler)
+
 	app.Run(iris.Addr(":8080"))
 }
